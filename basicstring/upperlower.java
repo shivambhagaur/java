e5840820b26c5d4cde216e5fdea0d602f71a32fc
shivambhagaur <shivambhagour8@gmail.com>
2025-08-18 18:46:14 +0530

    Create upperlower.java

---
+++ basicstring/upperlower.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class upperlower{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        // take a input from user 
+        String a = sc.nextLine();
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+    }
+        }
