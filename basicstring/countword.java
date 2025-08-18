07f7999badfc338f048cf7aa49c64b4111e7560c
shivambhagaur <shivambhagour8@gmail.com>
2025-08-18 20:18:49 +0530

    Create countword.java

---
+++ basicstring/countword.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class wordcount{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+       String countwords[] = a.split(" ");
+        System.out.println(countwords.length);
+    }
+        }
